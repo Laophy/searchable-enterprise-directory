ad6bc109e2c03d13de3f212f5cc75afdc95b849d
Alice Smith <alice@smith.com>
2023-07-13 16:57:39 -0400

    SalaryPrediction and compensation changes

---
+++ client/src/components/SalaryPrediction.js
@@ -0,0 +1,48 @@
+import {PythonShell} from 'react';
+import React, {useState} from'react';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+
+
+export function SalaryPrediction() {
+    const [jobRole, setJobRole] = React.useState('');
+
+    const[workLocation, setWorkLocation] = React.useState('');
+
+
+    return (
+        <Box sx={{ minWidth: 120 }}>
+          <FormControl fullWidth sx={{m:1,mt:3}}>
+            <InputLabel id="select-role-label">Job Role</InputLabel>
+            <Select
+              labelId="select-role-label"
+              id="select-role"
+              value={jobRole}
+              label="Job Role"
+              onChange={(e)=> setJobRole(e.target.value)}
+            >
+              <MenuItem value={"Data Engineer"}>Data Engineer</MenuItem>
+              <MenuItem value={20}>Twenty</MenuItem>
+              <MenuItem value={30}>Thirty</MenuItem>
+            </Select>
+            </FormControl>
+            <FormControl fullWidth sx={{m:1,mt:3}}>
+            <InputLabel id="select-location-label">Work Location</InputLabel>
+            <Select
+              labelId="select-location-label"
+              id="select-location"
+              value={workLocation}
+              label="Work Location"
+              onChange={(e)=> setWorkLocation(e.target.value)}
+            >
+              <MenuItem value={"Hartford"}>Hartford</MenuItem>
+              <MenuItem value={20}>Twenty</MenuItem>
+              <MenuItem value={30}>Thirty</MenuItem>
+            </Select>
+          </FormControl>
+        </Box>
+      );
+}
--- client/src/pages/account/Compensation.js
+++ client/src/pages/account/Compensation.js
@@ -1,14 +1,10 @@
 import Typography from '@mui/material/Typography';
+import { SalaryPrediction } from '../../components/SalaryPrediction';
 
 export function Compensation() {
     return (
         <div style={{ textAlign: 'center', width: '100%', marginTop: '10vh' }}>
-                <Typography variant="h3" noWrap component="div">
-                    Compensation
-                </Typography>
-                <Typography variant="p" noWrap component="div">
-                    This page is currently under <strong>development!</strong>
-                </Typography>
+            <SalaryPrediction/>
         </div>
     )
 }
