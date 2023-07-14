95997618f11c1e32cc63a5505b95a5c56e7aff6c
Alice Smith <alice@smith.com>
2023-07-14 10:53:34 -0400

    salaryPrediction changes

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 import express from 'express'
 import cors from 'cors'
 import * as Routes from './handleRoutes.js'
-
+import { Console } from 'console';
+import {spawn} from 'child_process';
+//const {spawn} = require('child_process');
 const port = 4000;
 const app = express();
 
@@ -22,7 +24,16 @@ app.get("/api/users/:id", function (req, res) {
     })
 });
 
+app.post('/api/predict/salary',(req,res) => {
+    const input = req.body
+    const data = JSON.stringify(input)
+    const pythonScript = spawn('python',['data_analysis.py'])
+    console.log(input)
+});
+
 app.use(express.static('./public'))
+
+
 
 console.log("Server starting on port: " + port);
 app.listen(port);
